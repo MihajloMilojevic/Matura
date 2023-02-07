ed2a370aef4dc572f2d04373086067ab26dba390
Mihajlo Milojevic <milojevicm374@gmail.com>
2023-02-08 00:20:38 +0100

    hosting

--- Praksa/Veb/B1/components/nav.php
+++ Praksa/Veb/B1/components/nav.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html lang="sr-RS">
-<head>
-    <link rel="stylesheet" href="../css/nav.css">
-</head>
 <body>
     <nav>
         <div class="links">
-            <a href="/" class="link <?php echo ($url == "index" ? "active" : "") ?>">Početna</a>
+            <a href="." class="link <?php echo ($url == "index" ? "active" : "") ?>">Početna</a>
             <a href="./autor.php" class="link <?php echo ($url == "autor" ? "active" : "") ?>">O autoru</a>
             <a href="./uputstvo.php" class="link <?php echo ($url == "uputstvo" ? "active" : "") ?>">Uputstvo</a>
         </div>
