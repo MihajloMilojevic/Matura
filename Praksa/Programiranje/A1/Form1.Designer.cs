4fb51aa65b54fd34d3db38e5961901e5c40d3ab2
Mihajlo Milojevic <milojevicm374@gmail.com>
2023-05-17 21:16:11 +0200

    A1

--- Praksa/Programiranje/A1/Form1.Designer.cs
+++ Praksa/Programiranje/A1/Form1.Designer.cs
@@ -30,10 +30,29 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.citaociTab = new System.Windows.Forms.TabPage();
+            this.izlazBTN = new System.Windows.Forms.Button();
+            this.upisiBtn = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.AdresaTB = new System.Windows.Forms.TextBox();
+            this.prezimeTB = new System.Windows.Forms.TextBox();
+            this.imeTB = new System.Windows.Forms.TextBox();
+            this.jmbgTB = new System.Windows.Forms.TextBox();
+            this.brojTB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.iznajmljivanjaTab = new System.Windows.Forms.TabPage();
             this.oAplikacijiTab = new System.Windows.Forms.TabPage();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
+            this.citaociTab.SuspendLayout();
             this.oAplikacijiTab.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,25 +65,183 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(585, 318);
+            this.tabControl1.Size = new System.Drawing.Size(1134, 399);
             this.tabControl1.TabIndex = 0;
             // 
             // citaociTab
             // 
-            this.citaociTab.Location = new System.Drawing.Point(4, 33);
+            this.citaociTab.Controls.Add(this.izlazBTN);
+            this.citaociTab.Controls.Add(this.upisiBtn);
+            this.citaociTab.Controls.Add(this.lista);
+            this.citaociTab.Controls.Add(this.AdresaTB);
+            this.citaociTab.Controls.Add(this.prezimeTB);
+            this.citaociTab.Controls.Add(this.imeTB);
+            this.citaociTab.Controls.Add(this.jmbgTB);
+            this.citaociTab.Controls.Add(this.brojTB);
+            this.citaociTab.Controls.Add(this.label5);
+            this.citaociTab.Controls.Add(this.label4);
+            this.citaociTab.Controls.Add(this.label3);
+            this.citaociTab.Controls.Add(this.label2);
+            this.citaociTab.Controls.Add(this.label1);
+            this.citaociTab.Location = new System.Drawing.Point(4, 38);
             this.citaociTab.Name = "citaociTab";
             this.citaociTab.Padding = new System.Windows.Forms.Padding(3);
-            this.citaociTab.Size = new System.Drawing.Size(577, 281);
+            this.citaociTab.Size = new System.Drawing.Size(1126, 357);
             this.citaociTab.TabIndex = 0;
             this.citaociTab.Text = "Čitaoci";
             this.citaociTab.UseVisualStyleBackColor = true;
+            this.citaociTab.Enter += new System.EventHandler(this.citaociTab_Enter);
+            // 
+            // izlazBTN
+            // 
+            this.izlazBTN.Location = new System.Drawing.Point(31, 296);
+            this.izlazBTN.Name = "izlazBTN";
+            this.izlazBTN.Size = new System.Drawing.Size(348, 43);
+            this.izlazBTN.TabIndex = 12;
+            this.izlazBTN.Text = "Izlaz";
+            this.izlazBTN.UseVisualStyleBackColor = true;
+            this.izlazBTN.Click += new System.EventHandler(this.izlazBTN_Click);
+            // 
+            // upisiBtn
+            // 
+            this.upisiBtn.Location = new System.Drawing.Point(31, 247);
+            this.upisiBtn.Name = "upisiBtn";
+            this.upisiBtn.Size = new System.Drawing.Size(348, 43);
+            this.upisiBtn.TabIndex = 11;
+            this.upisiBtn.Text = "Upiši čitaoca";
+            this.upisiBtn.UseVisualStyleBackColor = true;
+            this.upisiBtn.Click += new System.EventHandler(this.upisiBtn_Click);
+            // 
+            // lista
+            // 
+            this.lista.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lista.FullRowSelect = true;
+            this.lista.HideSelection = false;
+            this.lista.Location = new System.Drawing.Point(401, 34);
+            this.lista.MultiSelect = false;
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(707, 305);
+            this.lista.TabIndex = 10;
+            this.lista.UseCompatibleStateImageBehavior = false;
+            this.lista.SelectedIndexChanged += new System.EventHandler(this.lista_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Članska karta";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "JMBG";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Ime";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Prezime";
+            this.columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Adresa";
+            this.columnHeader5.Width = 100;
+            // 
+            // AdresaTB
+            // 
+            this.AdresaTB.Location = new System.Drawing.Point(138, 198);
+            this.AdresaTB.Name = "AdresaTB";
+            this.AdresaTB.Size = new System.Drawing.Size(241, 34);
+            this.AdresaTB.TabIndex = 9;
+            // 
+            // prezimeTB
+            // 
+            this.prezimeTB.Location = new System.Drawing.Point(138, 158);
+            this.prezimeTB.Name = "prezimeTB";
+            this.prezimeTB.Size = new System.Drawing.Size(241, 34);
+            this.prezimeTB.TabIndex = 8;
+            // 
+            // imeTB
+            // 
+            this.imeTB.Location = new System.Drawing.Point(138, 118);
+            this.imeTB.Name = "imeTB";
+            this.imeTB.Size = new System.Drawing.Size(241, 34);
+            this.imeTB.TabIndex = 7;
+            // 
+            // jmbgTB
+            // 
+            this.jmbgTB.Location = new System.Drawing.Point(138, 78);
+            this.jmbgTB.Name = "jmbgTB";
+            this.jmbgTB.Size = new System.Drawing.Size(241, 34);
+            this.jmbgTB.TabIndex = 6;
+            // 
+            // brojTB
+            // 
+            this.brojTB.Location = new System.Drawing.Point(224, 34);
+            this.brojTB.Name = "brojTB";
+            this.brojTB.Size = new System.Drawing.Size(155, 34);
+            this.brojTB.TabIndex = 5;
+            this.brojTB.TextChanged += new System.EventHandler(this.brojTB_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(26, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 34);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Adresa";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(26, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 34);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Prezime";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(26, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 34);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ime";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(26, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "JMBG";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(26, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Br. članske karte";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // iznajmljivanjaTab
             // 
-            this.iznajmljivanjaTab.Location = new System.Drawing.Point(4, 33);
+            this.iznajmljivanjaTab.Location = new System.Drawing.Point(4, 38);
             this.iznajmljivanjaTab.Name = "iznajmljivanjaTab";
             this.iznajmljivanjaTab.Padding = new System.Windows.Forms.Padding(3);
-            this.iznajmljivanjaTab.Size = new System.Drawing.Size(577, 281);
+            this.iznajmljivanjaTab.Size = new System.Drawing.Size(1126, 357);
             this.iznajmljivanjaTab.TabIndex = 1;
             this.iznajmljivanjaTab.Text = "Pregled Iznamljivanja";
             this.iznajmljivanjaTab.UseVisualStyleBackColor = true;
@@ -72,9 +249,9 @@
             // oAplikacijiTab
             // 
             this.oAplikacijiTab.Controls.Add(this.textBox1);
-            this.oAplikacijiTab.Location = new System.Drawing.Point(4, 33);
+            this.oAplikacijiTab.Location = new System.Drawing.Point(4, 38);
             this.oAplikacijiTab.Name = "oAplikacijiTab";
-            this.oAplikacijiTab.Size = new System.Drawing.Size(577, 281);
+            this.oAplikacijiTab.Size = new System.Drawing.Size(1126, 357);
             this.oAplikacijiTab.TabIndex = 2;
             this.oAplikacijiTab.Text = "O Aplikaciji";
             this.oAplikacijiTab.UseVisualStyleBackColor = true;
@@ -85,22 +262,25 @@
             this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(577, 281);
+            this.textBox1.Size = new System.Drawing.Size(1126, 357);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(585, 318);
+            this.ClientSize = new System.Drawing.Size(1134, 399);
             this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Biblioteka";
             this.tabControl1.ResumeLayout(false);
+            this.citaociTab.ResumeLayout(false);
+            this.citaociTab.PerformLayout();
             this.oAplikacijiTab.ResumeLayout(false);
             this.oAplikacijiTab.PerformLayout();
             this.ResumeLayout(false);
@@ -114,6 +294,24 @@
         private System.Windows.Forms.TabPage iznajmljivanjaTab;
         private System.Windows.Forms.TabPage oAplikacijiTab;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView lista;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.TextBox AdresaTB;
+        private System.Windows.Forms.TextBox prezimeTB;
+        private System.Windows.Forms.TextBox imeTB;
+        private System.Windows.Forms.TextBox jmbgTB;
+        private System.Windows.Forms.TextBox brojTB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button izlazBTN;
+        private System.Windows.Forms.Button upisiBtn;
     }
 }
 
--- Praksa/Programiranje/A1/Form1.cs
+++ Praksa/Programiranje/A1/Form1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace A1
 {
@@ -15,6 +18,160 @@ namespace A1
         public Form1()
         {
             InitializeComponent();
+        }
+        public SqlConnection konekcija;
+        public SqlDataAdapter adapter;
+        public DataTable tabela;
+        public SqlCommand komanda;
+        public void Konekcija()
+        {
+            konekcija = new SqlConnection();
+            konekcija.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MM_A1;Integrated Security=True;";
+            komanda = new SqlCommand();
+            komanda.Connection = konekcija;
+            tabela = new DataTable();
+            adapter = new SqlDataAdapter();
+        }
+        private void GetData()
+        {
+            Konekcija();
+            komanda.CommandText = "SELECT CitalacID, Maticni_broj, Ime, Prezime, Adresa FROM Citalac";
+            adapter.SelectCommand = komanda;
+            adapter.Fill(tabela);
+            konekcija.Close();
+        }
+        private void ShowList()
+        {
+            lista.Clear();
+
+            lista.Columns.Add("Članska karta", 130);
+            lista.Columns.Add("JMBG", 150);
+            lista.Columns.Add("Ime", 100);
+            lista.Columns.Add("Prezime", 100);
+            lista.Columns.Add("Adresa", 250);
+
+            lista.View = View.Details;
+            lista.GridLines = true;
+            lista.FullRowSelect = true;
+            for (int i = 0; i < tabela.Rows.Count; i++)
+            {
+                string[] red = {
+                    tabela.Rows[i][0].ToString(),
+                    tabela.Rows[i][1].ToString(),
+                    tabela.Rows[i][2].ToString(),
+                    tabela.Rows[i][3].ToString(),
+                    tabela.Rows[i][4].ToString(),
+                };
+                ListViewItem item = new ListViewItem(red);
+                lista.Items.Add(item);
+            }
+        }
+        private void ShowData(string id)
+        {
+            string[] data = Find(id);
+
+            jmbgTB.Enabled = data == null && id != "";
+            imeTB.Enabled = data == null && id != "";
+            prezimeTB.Enabled = data == null && id != "";
+            AdresaTB.Enabled = data == null && id != "";
+
+            upisiBtn.Enabled = data == null && id != "";
+
+            if (data == null)
+            {
+                jmbgTB.Text = "";
+                imeTB.Text = "";
+                prezimeTB.Text = "";
+                AdresaTB.Text = "";
+            }
+            else
+            {
+                jmbgTB.Text = data[1];
+                imeTB.Text = data[2];
+                prezimeTB.Text = data[3];
+                AdresaTB.Text = data[4];
+            }
+        }
+        private string[] Find(string id) {
+            for (int i = 0; i < tabela.Rows.Count; i++)
+            {
+                if(tabela.Rows[i][0].ToString() == id)
+                {
+                    string[] red = {
+                        tabela.Rows[i][0].ToString(),
+                        tabela.Rows[i][1].ToString(),
+                        tabela.Rows[i][2].ToString(),
+                        tabela.Rows[i][3].ToString(),
+                        tabela.Rows[i][4].ToString(),
+                    };
+                    return red;
+                }
+                
+            }
+            return null;
+        }
+        private void izlazBTN_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void citaociTab_Enter(object sender, EventArgs e)
+        {
+            GetData();
+            ShowList();
+            ShowData(brojTB.Text);
+
+        }
+
+        private void lista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lista.SelectedItems.Count > 0)
+            {
+                brojTB.Text = lista.SelectedItems[0].SubItems[0].Text.ToString();
+
+            }
+
+        }
+
+        private void brojTB_TextChanged(object sender, EventArgs e)
+        {
+            ShowData(brojTB.Text);
+        }
+
+        private void upisiBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Konekcija();
+                konekcija.Open();
+                komanda.CommandText = "INSERT INTO Citalac (CitalacID, Maticni_broj, Ime, Prezime, Adresa) VALUES(@CitalacID, @Maticni_broj, @Ime, @Prezime, @Adresa)";
+                komanda.Parameters.Add("@CitalacID", brojTB.Text);
+                komanda.Parameters.Add("@Maticni_broj", jmbgTB.Text);
+                komanda.Parameters.Add("@Ime", imeTB.Text);
+                komanda.Parameters.Add("@Prezime", prezimeTB.Text);
+                komanda.Parameters.Add("@Adresa", AdresaTB.Text);
+                komanda.ExecuteNonQuery();
+                konekcija.Close();
+                GetData();
+                ShowList();
+                ShowData(brojTB.Text);
+                MessageBox.Show(
+                        "Uspešno registrovan čitalac",
+                        "Uspeh",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                    );
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                        ex.Message,
+                        "Greška",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+            }
         }
     }
 }
