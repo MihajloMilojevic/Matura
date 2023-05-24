5c459238a2e38c9537f7add1c53655d11c535e51
Mihajlo Milojevic <milojevicm374@gmail.com>
2023-05-24 09:41:27 +0200

    B11

---
+++ Praksa/Veb/B11/components/nav.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="sr-RS">
+<body>
+    <nav>
+        <div class="links">
+            <a href="." class="link <?php echo ($url == "index" ? "active" : "") ?>">Početna</a>
+            <a href="./autor.php" class="link <?php echo ($url == "autor" ? "active" : "") ?>">O autoru</a>
+            <a href="./uputstvo.php" class="link <?php echo ($url == "uputstvo" ? "active" : "") ?>">Uputstvo</a>
+        </div>
+        <span class="zavrsni-ispit">Završni ispit</span>
+    </nav>
+</body>
+</html>
